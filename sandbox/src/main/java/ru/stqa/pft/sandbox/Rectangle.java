e3dc74e29753258defe348e6a0d5ecd6edeb5375
JulMann <julia.mannanova@gmail.com>
2020-02-24 16:34:17 +0300

    Создан класс Rectangle

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Rectangle.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.sandbox;
+
+/**
+ * Created by user on 24.02.2020.
+ */
+public class Rectangle {
+  public double a;
+  public double b;
+
+  public Rectangle (double a, double b){
+    this.a = a;
+    this.b = b;
+  }
+
+}
