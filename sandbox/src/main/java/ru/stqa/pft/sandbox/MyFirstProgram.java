fdb939bafe148b04eef9019bf9b008264a476d18
JulMann <julia.mannanova@gmail.com>
2020-02-28 00:31:24 +0300

    Исправление вычисления расстояния с помощью метода в классе Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -13,9 +13,11 @@ public class MyFirstProgram {
     Rectangle r = new Rectangle(4, 6);
     System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 
-    Point p = new Point(2, 5, 1, 10);
-    System.out.println("Расстояние между точками с координатами (" + p.x1 + ", " + p.y1 +
-            ") и (" + p.x2 + ", " + p.y2 + ")" + " равно " + p.distance());
+    Point p1 = new Point(2, 5);
+    Point p2 = new Point(1, 10);
+
+    System.out.println("Расстояние между точками с координатами (" + p1.x + ", " + p1.y +
+            ") и (" + p2.x + ", " + p2.y + ")" + " равно " + p1.distance(p2));
 
   }
 
